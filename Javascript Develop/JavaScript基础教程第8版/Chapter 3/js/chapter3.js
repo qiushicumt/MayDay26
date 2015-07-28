93ac95eed9dadd1d582ec731e57cd241e88303f1
qiushicumt <qiushicumt@gmail.com>
2015-07-28 17:36:49 +0800

    JavaScript Chapter3 Commit2
    2015-7-28 MayDay26-PC

--- Javascript Develop/JavaScript基础教程第8版/Chapter 3/js/chapter3.js
+++ Javascript Develop/JavaScript基础教程第8版/Chapter 3/js/chapter3.js
@@ -8,7 +8,6 @@ function initAll() {
 		document.getElementById("square" + i).innerHTML = num;
 	}
 }
-*/
 // 3-4 脚本
 window.onload = initAll;
 function initAll() {
@@ -21,4 +20,126 @@ function setNum(squareNum) {
 	var currentId = "square" + squareNum;
 	var num = Math.floor(Math.random() * 75) + 1;
 	document.getElementById(currentId).innerHTML = num;
+}
+
+// 3-6 脚本
+window.onload = initAll;
+function initAll() {
+	if(document.getElementById) {
+		document.getElementById("reload").onclick = reloadWeb;  // 对超链接进行重定向
+		for (var i=0; i<24; i++) {
+			setNum(i);
+		}
+	}
+	else {
+		alert("Your browser don't support the javascript!");
+	}
+}
+function setNum(squareNum) {
+	var squareId = "square" + squareNum;
+	var colPlace = new Array(0,0,0,0,0,1,1,1,1,1,2,2,2,2,3,3,3,3,3,4,4,4,4,4);	  
+	var Num = colPlace[squareNum] * 15 + Math.floor(Math.random() * 15) + 1; 	// 根据数组中的数字乘以15，用于确定每一列的数值范围的最大值
+	document.getElementById(squareId).innerHTML = Num;
+}
+function reloadWeb() {
+	window.location = "BingoCard.html";
+	return false;
+}
+
+// 3-7 脚本
+window.onload = initAll;
+function initAll() {
+	if(document.getElementById) {
+		document.getElementById("reload").onclick = reloadWeb;
+		for(var i = 0; i < 24; i++) {
+			setNum(i);
+		}
+	}
+	else {
+		alert("Your browser don't support this javascript!");
+	}
+}
+function setNum(squareNum) {
+	var squareId = "square" + squareNum; 
+	var colPlace = new Array(0,0,0,0,0,1,1,1,1,1,2,2,2,2,3,3,3,3,3,4,4,4,4,4);
+	var Num = colPlace[squareNum] * 15 + getRandom() + 1;		// getRandom()函数用于获取0~15随机数
+	document.getElementById(squareId).innerHTML = Num;
+}
+function getRandom() {
+	return Math.floor(Math.random() * 15); 		// 函数生成一个0~15的随机数并返回
+}
+function reloadWeb() {
+	window.location = "BingoCard.html";
+	return false;
+}
+
+// 3-8 脚本
+window.onload = initAll;
+var usedNums = new Array(76);		// 声明一个数组，用于存放每个数字的状态
+function initAll() {
+	if(document.getElementById) {
+		document.getElementById("reload").onclick = reloadWeb;
+		for(var i = 0; i < 24; i++) {
+			setNum(i);
+		}
+	}
+	else {
+		alert("Your browser don't support this script!");
+	}
+}
+function setNum(squareId) {
+	var currentId = "square" + squareId;
+	var colPlace = new Array(0,0,0,0,0,1,1,1,1,1,2,2,2,2,3,3,3,3,3,4,4,4,4,4);
+	var newNum = colPlace[squareId] * 15 + getRandom() + 1;
+
+	// 使用if...else...语句用于对获得的随机数进行状态判断，若之前未产生此随机数，则将此随机数的状态置为1，
+	// 存入usedNums数组中，然后将此随机数填入空格内。若之前已经产生此随机数，则转到else语句，调用自身，重
+	// 新进行随机数的产生。
+	if(!usedNums[newNum]) {	
+		usedNums[newNum] = true;
+		document.getElementById(currentId).innerHTML = newNum;
+	}
+	else {
+		setNum(squareId);		// 使用递归调用自身，给空格处填上数字
+	}
+}
+function getRandom() {
+	return Math.floor(Math.random() * 15);
+}
+function reloadWeb() {
+	window.location = "BingoCard.html";
+	return false;
+}
+*/
+// 3-9 脚本
+window.onload = initAll;
+usedNums = new Array(76);
+function initAll() {
+	if(document.getElementById) {
+		document.getElementById("reload").onclick = reloadWeb;
+		for(var i = 0; i < 24; i++) {
+			setNum(i);
+		}
+	}
+	else {
+		alert("Your browser don't support this script!");
+	}
+}
+function setNum(squareId) {
+	var currentId = "square" + squareId;
+	var colPlace = new Array(0,0,0,0,0,1,1,1,1,1,2,2,2,2,3,3,3,3,3,4,4,4,4,4);
+
+	do {
+		var newNum = colPlace[squareId] * 15 + getRandom() + 1;
+	}while(usedNums[newNum]);
+
+	usedNums[newNum] = true;
+	document.getElementById(currentId).innerHTML = newNum;
+}
+function getRandom() {
+	return Math.floor(Math.random() * 15);
+}
+function reloadWeb() {
+	window.location = "BingoCard.html";
+	return false;
 }
