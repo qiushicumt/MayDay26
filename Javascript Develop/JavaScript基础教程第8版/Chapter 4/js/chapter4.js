d3762b3d587613c73f2030989eae20b0d7200395
qiushicumt <qiushicumt@gmail.com>
2015-08-10 16:56:07 +0800

    JavaScript Chapter4 Commit 1
    2015-8-10 MayDay26-PC

---
+++ Javascript Develop/JavaScript基础教程第8版/Chapter 4/js/chapter4.js
@@ -0,0 +1,7 @@
+// 4-1 脚本
+function changeRed() {
+	document.getElementById("arrow").src = '../images/arrow_on.gif';
+}
+function changeBlack() {
+	document.getElementById("arrow").src = '../images/arrow_off.gif'
+}
