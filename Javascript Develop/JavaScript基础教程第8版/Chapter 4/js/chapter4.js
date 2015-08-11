992730ae1e20f016ab3fa165a15b47b88a1f1d97
qiushicumt <qiushicumt@gmail.com>
2015-08-11 17:28:17 +0800

    JavaScript Chapter4 Commit2
    2015-8-11 MayDay26-PC

--- Javascript Develop/JavaScript基础教程第8版/Chapter 4/js/chapter4.js
+++ Javascript Develop/JavaScript基础教程第8版/Chapter 4/js/chapter4.js
@@ -1,7 +1,42 @@
+/*
 // 4-1 脚本
 function changeRed() {
 	document.getElementById("arrow").src = '../images/arrow_on.gif';
 }
 function changeBlack() {
-	document.getElementById("arrow").src = '../images/arrow_off.gif'
+	document.getElementById("arrow").src = '../images/arrow_off.gif';
+}
+*/
+// 4-2 脚本
+function changeRed() {
+	document.images['arrow'].src = '../images/arrow_on.gif';
+}
+function changeBlack() {
+	document.images['arrow'].src = '../images/arrow_off.gif';
+}
+
+// 4-3 脚本
+window.onload = rolloverInit;
+function rolloverInit() {
+	// document.images, images集合返回当前文档中所有图片的数组
+	for(var i = 0; i < document.images.length; i++) {
+		if (document.images[i].parentNode.tagName == "A") {
+			setupRollover(document.images[i]);
+		}
+	}
+}
+function setupRollover(thisImage) {
+
+	thisImage.outImage = new Image();
+	thisImage.outImage.src = thisImage.src;
+	thisImage.onmouseout = function() {
+		this.src = this.outImage.src;
+	}
+
+	thisImage.overImage = new Image();
+	// alert(thisImage.id);
+	thisImage.overImage.src = "../images/" + thisImage.id + "_on.gif";
+	thisImage.onmouseover = function() {
+		this.src = this.overImage.src;
+	}
 }
