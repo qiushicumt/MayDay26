2a2805c4c6793d4ccaa01c4e04699064118a266f
qiushicumt <qiushicumt@gmail.com>
2015-08-17 15:47:57 +0800

    Xuanxiangka Commit2
    2015-8-17 MayDay26-PC JavaScript Practise

--- Javascript Develop/JS Practise/js/xuanxiangka.js
+++ Javascript Develop/JS Practise/js/xuanxiangka.js
@@ -1,4 +1,36 @@
 // 1-1 选项卡脚本1
 window.onload = function() {
+	var inputAll = document.getElementsByTagName("input");
+	// inputAll[0].onclick = function() {inputAll[0].style.background = "#ff8400";}
+	
+	for(var i = 0; i < inputAll.length; i++) {
+		inputAll[i].onclick = function() {
+			alert(i);
+			inputAll[i].style.background = "#ff8400";
+		}
+	}
 	
 }
+
+/*
+window.onload = initAll;
+function initAll() {
+	var oInput = document.getElementsByTagName("input");
+	var oDiv = document.getElementsByTagName("div");
+	for(i = 0; i < oInput.length; i++) {
+		oInput
+	}
+	
+	oInput[2].onclick = function() {
+		oInput[2].style.backgroundColor = "#ff8400";
+		alert(oInput[1].value);
+		alert(oInput.length);
+	}
+	// for(var i = 0; i < oInput.length; i++) {
+	// 	oInput[i].onclick = function() {
+	// 		alert(oInput[i].value);
+	// 		alert(oInput[i].value);
+	// 	}
+	// }
+}
+*/
