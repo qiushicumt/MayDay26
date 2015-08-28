98aef0266dfcd064598d7f06d2998a8a103d3030
qiushicumt <qiushicumt@gmail.com>
2015-08-28 17:33:58 +0800

    JS Practise Wufeng Commit2
    2015-8-28 MayDay-PC

--- Javascript Develop/JS Practise/js/wufeng.js
+++ Javascript Develop/JS Practise/js/wufeng.js
@@ -1,3 +1,36 @@
 window.onload = function() {
+	oUl = document.getElementsByTagName("ul");
+	oDiv = document.getElementById("wrappage");
+	var timer;
+	oUl[0].innerHTML = oUl[0].innerHTML + oUl[0].innerHTML;
+	console.log(oUl[0].innerHTML);
 
+	// function moveFunc() {
+	// 	oUl[0].style.left = (parseInt(oUl[0].style.left) +20) + "px";
+	// }
+	// setTimeout(moveFunc, 1000);
+
+	
+	function moveFunc() {
+		oUl[0].style.left = (parseInt(oUl[0].style.left) +2) + "px"; 
+	}
+	timer = setInterval(moveFunc, 20);
+	
 }
+
+
+/*
+// 无缝滚动脚本 1
+window.onload = function() {
+	var oUl = document.getElementsByTagName("ul");
+	var oDiv = document.getElementById("wrappage");
+	var timer;
+	
+	oUl[0].style.left = -parseInt(window.getComputedStyle(oDiv).width) + "px";	
+	function moveFunc() {
+		oUl[0].style.left = (parseInt(oUl[0].style.left) + 2) + "px";
+	}
+
+	timer = setInterval(moveFunc, 20);
+}
+*/
