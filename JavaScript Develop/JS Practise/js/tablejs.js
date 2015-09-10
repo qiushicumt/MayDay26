f6e5a37d9dfc95e37ffcdc9fc0aea5cdfcd09b87
qiushicumt <qiushicumt@gmail.com>
2015-09-10 17:36:30 +0800

    JavaScript Practise TableJS Commit3
    2015-9-10 MayDay26-PC

--- JavaScript Develop/JS Practise/js/tablejs.js
+++ JavaScript Develop/JS Practise/js/tablejs.js
@@ -1,4 +1,87 @@
-// table脚本2
+// table 脚本4
+window.onload = function() {
+	oDiv = document.getElementById("inputDIV");
+	oInput = oDiv.getElementsByTagName("input");
+	oTab = document.getElementById("myTable");
+	oRow = oTab.tBodies[0].rows;
+
+	oInput[2].onclick = function() {
+		var oTr = document.createElement("tr");
+
+		var oTd = document.createElement("td");
+		oTd.innerHTML = oRow.length + 1;
+		oTr.appendChild(oTd);
+
+		var oTd = document.createElement("td");
+		oTd.innerHTML = oInput[0].value;
+		oTr.appendChild(oTd);
+
+		var oTd = document.createElement("td");
+		oTd.innerHTML = oInput[1].value;
+		oTr.appendChild(oTd);
+
+		var oTd = document.createElement("td");
+		oTd.innerHTML = "<a href=\"javascript:;\">删除</a>";
+		oTr.appendChild(oTd);
+
+		oTab.tBodies[0].appendChild(oTr);
+	}
+	for(var i = 0; i < oRow.length; i++) {
+		
+	}
+}
+
+/*
+// table 脚本3
+window.onload = function() {
+	createRows();
+	rowsColorChange();
+}
+function createRows() {
+	var oDiv = document.getElementById("inputDIV");
+	var oInput = oDiv.getElementsByTagName("input");
+	var oTab = document.getElementById("myTable");
+	var oRow = oTab.tBodies[0].rows;
+	oInput[2].onclick = function() {
+		var oTr = document.createElement("tr");
+
+		var oTd = document.createElement("td");
+		oTd.innerHTML = oRow.length + 1;
+		oTr.appendChild(oTd);
+
+		var oTd = document.createElement("td");
+		oTd.innerHTML = oInput[0].value;
+		oTr.appendChild(oTd);
+
+		var oTd = document.createElement("td");
+		oTd.innerHTML = oInput[1].value;
+		oTr.appendChild(oTd);
+
+		oTab.tBodies[0].appendChild(oTr);
+	}
+}
+function rowsColorChange() {
+	var oTab = document.getElementById("myTable");
+	var oRow = oTab.tBodies[0].rows;
+	var oldColor;
+	for(var i = 0; i < oRow.length; i++) {
+		if(i%2) {
+			oRow[i].style.background = "#ccc";
+		}
+		else {
+			oRow[i].style.background = "";
+		}
+		oRow[i].onmouseover = function() {
+			oldColor = this.style.background;
+			this.style.background = "rgb(134,219,34)";
+		}
+		oRow[i].onmouseout = function() {
+			this.style.background = oldColor;
+		}
+	}
+}
+
+// table 脚本2
 window.onload = function() {
 	rowsColorChange();
 	createRows();
@@ -50,8 +133,8 @@ function createRows() {
 		oTab.tBodies[0].appendChild(oTr);
 	}
 }
-/*
-// table脚本1
+
+// table 脚本1
 window.onload = function() {
 	var oTab = document.getElementById("myTable");
 	var oRow = oTab.tBodies[0].rows;
