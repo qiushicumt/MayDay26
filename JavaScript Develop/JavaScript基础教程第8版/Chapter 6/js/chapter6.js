917a6d28453227906cab179177232843c71f19dc
qiushicumt <qiushicumt@gmail.com>
2015-09-15 10:42:41 +0800

    JavaScript Chapter6 Commit2
    2015-9-15 MayDay26-PC

--- JavaScript Develop/JavaScript基础教程第8版/Chapter 6/js/chapter6.js
+++ JavaScript Develop/JavaScript基础教程第8版/Chapter 6/js/chapter6.js
@@ -1,3 +1,23 @@
+
+/*
+// 6-2 脚本
+window.onload = function() {
+	document.getElementById("months").selectedIndex = 0;	// selectedIndex选中的选项的index
+	document.getElementById("months").onchange = populateDays;
+}
+function populateDays() {
+	var monthDays = new Array(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
+	var monthStr = this.options[this.selectedIndex].value;
+	if(monthStr != "") {
+		var theMonth = parseInt(monthStr);
+		document.getElementById("days").options.length = 0;
+		for(var i = 0; i < monthDays[theMonth]; i++) {
+			document.getElementById("days").options[i] = new Option(i+1);
+		}
+	}
+
+}
+
 // 6-1 脚本
 window.onload = initForm;
 window.onunload = function() {}
@@ -12,3 +32,4 @@ function jumpPage() {
 		window.location = newPage;
 	}
 }
+*/
