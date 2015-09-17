0ed2d4c4e3053e3992dc6675d880115ae7b01e36
qiushicumt <qiushicumt@gmail.com>
2015-09-17 17:21:38 +0800

    JavaScript Chapter6 Commit4
    2015-9-17 MayDay26-PC

--- JavaScript Develop/JavaScript基础教程第8版/Chapter 6/js/chapter6.js
+++ JavaScript Develop/JavaScript基础教程第8版/Chapter 6/js/chapter6.js
@@ -1,9 +1,80 @@
+// 6-3 脚本
+window.onload = initForm;
+function initForm() {
+	var oForm = document.getElementById("myForm");
+	oForm.onsubmit = function() {
+		var allGood = true;
+		var allTags = document.getElementsByTagName("*");
+		for(var i = 0; i < allTags.length; i++) {
+			if(!validTag(allTags[i])) {
+				allGood = false;
+			}
+		}
+		return allGood;
+		function validTag(thisTag) {
+			var outClass = "";
+			var allClasses = thisTag.className.split(" ");	
+			for(var j = 0; j < allClasses.length; j++) {
+				outClass += validBasedOnClass(allClasses[j]) + " ";
+			}
+			thisTag.className = outClass;
+			// 在此处，if的判断条件中对class属性进行判断，包含invalid则返回索引值，不包含则返回-1
+			if(outClass.indexOf("invalid") > -1) {		
+				thisTag.focus();
+				if(thisTag.nodeName == "INPUT") {
+					thisTag.select();
+				}
+				return false;
+			}
+			return true;
+			function validBasedOnClass(thisClass) {
+				var classBack = "";
+				switch(thisClass) {
+					case "":
+					case "invalid":
+						break;
+					case "reqd":
+						if(allGood && thisTag.value === "") {
+							classBack = "invalid";
+						}
+						classBack += thisClass;
+						break;
+					default:
+						if(allGood && !corssCheck(thisTag, thisClass)) {
+							classBack = "invalid";
+						}
+						classBack += thisClass;
+				}
+				return classBack;
+			}
+			function corssCheck(inTag, otherFieldID) {
+				if(!document.getElementById(otherFieldID)) {
+					return false;
+				}
+				return (inTag.value == document.getElementById(otherFieldID).value);
+			}
+		}
+	}
+}
+/*
 // 6-2 脚本2
 window.onload = function() {
 	document.getElementById("months").selectedIndex = 0;
-	document.getElementById("months").
+	// document.getElementById("months").onchange = function() { populateDays(); }
+	document.getElementById("months").onchange = populateDays;
 }
-/*
+function populateDays() {
+	var daysArray = new Array(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
+	var monthStr = this.options[this.selectedIndex].value;
+	if(monthStr != "") {
+		var theMonth = parseInt(monthStr);
+		document.getElementById("days").options.length = 0;
+		for(var i = 0; i < daysArray[theMonth]; i++) {
+			document.getElementById("days").options[i] = new Option(i+1);
+		}
+	}
+}
+
 // 6-2 脚本1
 window.onload = function() {
 	document.getElementById("months").selectedIndex = 0;	// selectedIndex选中的选项的index
