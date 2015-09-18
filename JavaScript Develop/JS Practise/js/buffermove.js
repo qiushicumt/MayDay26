31a654d196946b632354c76f478f876cdada8fc6
qiushicumt <qiushicumt@gmail.com>
2015-09-18 17:24:40 +0800

    JavaScript Practise BufferMove Commit2
    2015-9-18 MayDay26-PC

--- JavaScript Develop/JS Practise/js/buffermove.js
+++ JavaScript Develop/JS Practise/js/buffermove.js
@@ -1,3 +1,78 @@
+// 侧边栏淡入淡出--缓冲方式
+window.onload = function() {
+	var oDiv = document.getElementById("div1");
+	oDiv.style.left = -160 + "px";
+	oDiv.onmouseover = function() {
+		moveFunc(0);
+	}
+	oDiv.onmouseout = function() {
+		moveFunc(-160);
+	}
+}
+var timer = null;
+function moveFunc(iTarget) {
+	clearInterval(timer);
+	var oDiv = document.getElementById("div1");
+	timer = setInterval(function(){
+		var speed = (iTarget - oDiv.offsetLeft)/10;
+		speed = speed>0?Math.ceil(speed):Math.floor(speed);
+		if(oDiv.offsetLeft == iTarget) {
+			clearInterval(timer);
+			// alert("ok1");
+		}
+		else {
+			oDiv.style.left = oDiv.offsetLeft + speed + "px";
+			// alert("ok2");
+		}	
+	}, 30);
+}
+/*
+// 右侧悬浮窗缓冲运动--中间
+window.onscroll = function() {
+	var oDiv = document.getElementById("div1");
+	var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+	moveFunc(parseInt((document.documentElement.clientHeight - oDiv.offsetHeight)/2 + scrollTop));
+}
+var timer = null;
+function moveFunc(iTarget) {
+	clearInterval(timer);
+	var oDiv = document.getElementById("div1");
+	timer = setInterval(function(){
+		var speed = (iTarget - oDiv.offsetTop)/6;
+		speed = speed>0 ? Math.ceil(speed) : Math.floor(speed);
+		if(oDiv.offsetTop == iTarget) {
+			clearInterval(timer);
+		}
+		else {
+			oDiv.style.top = oDiv.offsetTop + speed + "px";
+			console.log(oDiv.offsetTop);
+		}
+	},30);
+}
+
+// 右侧悬浮窗口缓冲运动--右下角
+window.onscroll = function() {
+	var oDiv = document.getElementById("div1");
+	var scrollTop = document.documentElement.scrollTop||document.body.scrollTop;
+	moveFunc(document.documentElement.clientHeight - oDiv.offsetHeight + scrollTop);
+}
+var timer = null;
+function moveFunc(iTarget) {
+	clearInterval(timer);
+	var oDiv = document.getElementById("div1");
+	timer = setInterval(function() {
+		var speed = (iTarget - oDiv.offsetTop)/6;
+		speed = speed>0 ? Math.ceil(speed) : Math.floor(speed);
+		if(oDiv.offsetTop == iTarget) {
+			clearInterval("timer");
+		}
+		else {
+			oDiv.style.top = oDiv.offsetTop + speed + "px";
+		}
+	}, 30);
+}
+
+// div缓冲运动脚本
 window.onload = function() {
 	var oDiv = document.getElementById("div1");
 	var oBtn = document.getElementById("btn1");
@@ -27,3 +102,4 @@ window.onload = function() {
 		}, 30);
 	}
 }
+*/
