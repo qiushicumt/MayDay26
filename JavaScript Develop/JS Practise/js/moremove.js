b19a92e7902fa24877bb2bc57d28286e5c6ef248
qiushicumt <zhongqiushi@zhongqiushideMacBook-Air.local>
2015-09-19 17:48:34 +0800

    JavaScript Practise MoreMove Commit2
    2015-9-19 MayDay26-Mac

--- JavaScript Develop/JS Practise/js/moremove.js
+++ JavaScript Develop/JS Practise/js/moremove.js
@@ -1,3 +1,37 @@
+// 脚本3  多个div的颜色淡入淡出效果
+window.onload = function() {
+	var theDiv = document.getElementById("wrappage");
+	var oDiv = theDiv.getElementsByTagName("div");
+	for(var i = 0; i < oDiv.length; i++) {
+		oDiv[i].timer = null;
+		oDiv[i].onmouseover = function() {
+			moveFunc(this, 100);
+		}
+		oDiv[i].onmouseout = function() {
+			moveFunc(this, 30);
+		}
+	}
+}
+var alpha = 30;
+function moveFunc(obj, iTarget) {
+	clearInterval(obj.timer);
+	obj.timer = setInterval(function(){
+		var speed = (iTarget - alpha)/6;
+		speed = speed>0 ? Math.ceil(speed) : Math.floor(speed);
+		if(alpha == iTarget) {
+			clearInterval(obj.timer);
+		}
+		else {
+			// alpha += speed;
+			// 错误 obj.style.opacity = alpha + speed;
+			// 错误原因是 alpha+speed只是改变了一次当前div的透明度值，而alpha变量自身并没有发生变化 
+			alpha += speed;
+			obj.style.filter = "alpha(opacity:" + alpha + ")";
+			obj.style.opacity = alpha/100;
+		}
+	}, 30);
+}
+/*
 // 脚本2  各自触发事件使用各自的定时器
 window.onload = function() {
 	var theDiv = document.getElementById("wrappage");
@@ -26,7 +60,7 @@ function moveFunc(obj, iTarget) {
 		}
 	}, 30);
 }
-/*
+
 // 脚本1  三个触发事件使用同一个定时器
 window.onload = function() {
 	var oDiv = document.getElementById("wrappage");
