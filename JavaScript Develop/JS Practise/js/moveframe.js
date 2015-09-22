a3c7b09bf26e7893066d0356e6d01ab4d9d62a8f
qiushicumt <qiushicumt@gmail.com>
2015-09-22 11:12:18 +0800

    JavaScript Practise MoveFrame Commit3
    2015-9-22 MayDay26-PC

--- JavaScript Develop/JS Practise/js/moveframe.js
+++ JavaScript Develop/JS Practise/js/moveframe.js
@@ -1,24 +1,22 @@
-// 多物体、任意属性运动 最终脚本
+// 多物体、任意值运动框架最终脚本
 window.onload = function() {
-	var theDiv = document.getElementById("wrappage");
-	var oDiv = theDiv.getElementsByTagName("div");
-	for(var i = 0; i < oDiv.length; i++) {
-		oDiv[i].timer = null;
-	}
-	oDiv[0].onmouseover = function() {moveFunc(this, 'height', 400);}
-	oDiv[0].onmouseout = function() {moveFunc(this, 'height', 150);}
-	oDiv[1].onmouseover = function() {moveFunc(this, 'width', 400);}
-	oDiv[1].onmouseout = function() {moveFunc(this, 'width', 150);}
-	oDiv[2].onmouseover = function() {moveFunc(this, 'fontSize', 30);}
-	oDiv[2].onmouseout = function() {moveFunc(this, 'fontSize', 14);}
-	oDiv[3].onmouseover = function() {moveFunc(this, 'borderWidth', 30);}
-	oDiv[3].onmouseout = function() {moveFunc(this, 'borderWidth', 4);}
-	oDiv[4].onmouseover = function() {moveFunc(this, 'opacity', 100);}
-	oDiv[4].onmouseout = function() {moveFunc(this, 'opacity', 30);}
+	var theDiv = document.getElementById('wrappage');
+	var oDiv = theDiv.getElementsByTagName('div');
+
+	oDiv[0].onmouseover = function() { moveFunc(this, 'height', 400); }
+	oDiv[0].onmouseout = function() { moveFunc(this, 'height', 150); }
+	oDiv[1].onmouseover = function() { moveFunc(this, 'width', 400); }
+	oDiv[1].onmouseout = function() { moveFunc(this, 'width', 150); }
+	oDiv[2].onmouseover = function() { moveFunc(this, 'fontSize', 30); }
+	oDiv[2].onmouseout = function() { moveFunc(this, 'fontSize', 14); }
+	oDiv[3].onmouseover = function() { moveFunc(this, 'borderWidth', 30); }
+	oDiv[3].onmouseout = function() { moveFunc(this, 'borderWidth', 5); }
+	oDiv[4].onmouseover = function() { moveFunc(this, 'opacity', 100); }
+	oDiv[4].onmouseout = function() { moveFunc(this, 'opacity', 30); }
 }
 function getStyle(obj, attr) {
 	if(obj.currentStyle) {
-		return obj.currentStyle(attr);
+		return obj.currentStyle[attr];
 	}
 	else {
 		return getComputedStyle(obj, null)[attr];
@@ -26,9 +24,10 @@ function getStyle(obj, attr) {
 }
 function moveFunc(obj, attr, iTarget) {
 	clearInterval(obj.timer);
-	obj.timer = setInterval(function() {
+	obj.timer = setInterval(function(){
 		var cur;
 		if(attr == 'opacity') {
+			// cur = parseFloat(getStyle(obj, attr)) * 100;
 			cur = Math.round(getStyle(obj, attr)*100);
 		}
 		else {
@@ -43,6 +42,7 @@ function moveFunc(obj, attr, iTarget) {
 			if(attr == 'opacity') {
 				obj.style['filter'] = 'alpha(opacity:' + (cur + speed) + ');';
 				obj.style[attr] = (cur + speed)/100;
+				console.log(obj.style[attr]);
 			}
 			else {
 				obj.style[attr] = cur + speed + 'px';
@@ -50,48 +50,3 @@ function moveFunc(obj, attr, iTarget) {
 		}
 	}, 30);
 }
-/*
-window.onload = function() {
-	var theDiv = document.getElementById("wrappage");
-	var oDiv = theDiv.getElementsByTagName("div");
-	for(var i = 0; i < oDiv.length; i++) {
-		oDiv[i].timer = null;
-	}
-	// 第一个div变高
-	oDiv[0].onmouseover = function() {
-		moveFunc(this, "height", 400);
-	}
-	oDiv[0].onmouseout = function() {
-		moveFunc(this, "height", 150);
-	}
-	// 第二个div变宽
-	oDiv[1].onmouseover = function() {
-		moveFunc(this, 'width', 400);
-	}
-	oDiv[1].onmouseout = function() {
-		moveFunc(this, "width", 150);
-	}
-}
-function getStyle(obj, name) {	
-	if(obj.currentStyle) {				
-		return obj.currentStyle[name];		
-	}
-	else {
-		return getComputedStyle(obj, null)[name];
-	}
-}
-function moveFunc(obj, styleName, iTarget) {
-	clearInterval(obj.timer);	
-	obj.timer = setInterval(function(){
-		var cur = parseInt(getStyle(obj, styleName));
-		var speed = (iTarget - cur)/6;
-		speed = speed>0 ? Math.ceil(speed) : Math.floor(speed);
-		if(cur == iTarget) {
-			clearInterval(obj.timer);
-		}
-		else {
-			obj.style[styleName] = cur + speed + "px";
-		}
-	}, 30);
-}
-*/
