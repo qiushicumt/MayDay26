5fb4d2fa348af4681ec46ce1338191e75a1b0b09
qiushicumt <qiushicumt@gmail.com>
2015-10-16 17:35:03 +0800

    JavaScript Eloquent Chapter6 Commit1

---
+++ JavaScript Develop/Eloquent JavaScript/js/chapter6.js
@@ -0,0 +1,48 @@
+﻿// JavaScript source code
+var thePlan = [
+            "############################",
+            "#       #    #       o    ##",
+            "#                          #",
+            "#           #####          #",
+            "##          #   #    ##    #",
+            "###            ##     #    #",
+            "#            ###      #    #",
+            "#   ####                   #",
+            "#   ##        o            #",
+            "# o  #          o      ### #",
+            "#    #                     #",
+            "############################"
+];
+function Point(x, y) {
+    this.x = x;
+    this.y = y;
+}
+Point.prototype.add = function (other) {
+    return new Point((this.x + other.x), (this.y + other.y));
+}
+function Grid(width, height) {
+    this.width = width;
+    this.height = height;
+    this.cells = new Array(width * height);
+}
+Grid.prototype.valueAt = function (point) {
+    return this.cells[point.y * this.width + point.x];
+}
+Grid.prototype.setValueAt = function (point, value) {
+    this.cells[point.y * this.width + point.x] = value;
+}
+Grid.prototype.isInside = function (point) {
+    return point.x >= 0 && point.y >= 0 && point.x < this.width && point.y < this.height;
+}
+Grid.prototype.moveValue = function (from, to) {
+    this.setValueAt(to, this.valueAt(from));
+    this.setValueAt(from, undefined);
+}
+Grid.prototype.each = function (action) {
+    for (var y = 0; y < this.height; y++) {
+        for (var x = 0; x < this.width; x++) {
+            var point = new Point(x, y);
+            action(point, this.valueAt(point));
+        }
+    }
+}
