2a97eed1171cac1dbd7765213c6b00e1e12fe498
qiushicumt <qiushicumt@gmail.com>
2015-10-27 23:39:49 +0800

    C#And.Net Basis Day02 Commit2
    2015-10-27 MayDay26-PC

---
+++ C#And.Net Workspace/C#基础/Day02/15-leixingzhuanhuan/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_leixingzhuanhuan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  C#中等号的两侧数据类型应当一致，当不一致时，可以发生隐式类型转换和显示类型转换
+
+            //  隐式类型转换发生的条件：（1）、两种类型是兼容的，如int和double都为数字类型；
+            //  （2）、目标类型大于源类型，如double>int，此时，int型可以自动转换为double型；
+
+            //  通过语句，可以将大的数据转换成小的数据，显示类型转换发生的条件：（1）、两种类型是兼容的；
+            //  （2）、通过(目标类型)源类型进行显示类型转换，如 int n = (int)double;
+
+            //int num = 10;
+            //double d = num;
+            //double e = 28;
+            //num = (int)e;
+            //Console.WriteLine(d);
+            //Console.WriteLine(num);
+            //Console.ReadKey();
+
+            //  当表达式中有一个操作数的类型提升为更高一级的数值类型时，整个表达式的值也会提升为高一级的数值类型
+            int num1 = 10;
+            int num2 = 3;
+            //  double d = num1 / num2;
+            double d = num1 * 1.0 / num2;
+            Console.WriteLine(d);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#And.Net Workspace/C#基础/Day02/Practise/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            //  练习1、根据输入的天数（46），输出是几周几天
+            int day = 46;
+            int oWeek, oDay;
+            oWeek = day / 7 + 1;
+            oDay = day % 7;
+            Console.WriteLine("46天是第{0}周第{1}天", oWeek, oDay);
+            Console.ReadKey();
+            */
+            //  练习2、编程实现107653秒是几天几小时几分几秒
+            int oTime = 187653;
+            int oDay, oHour, oMinute, oSecond;
+            int temp;
+            oSecond = oTime % 60;
+            temp = oTime / 60;
+            oMinute = temp % 60;
+            temp = temp / 60;
+            oHour = temp % 24;
+            temp = temp / 24;
+            oDay = temp % 24;
+            Console.WriteLine("{0}秒是{1}天{2}小时{3}分钟{4}秒",oTime,oDay,oHour,oMinute,oSecond);
+            Console.ReadKey();
+        }
+    }
+}
