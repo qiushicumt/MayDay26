33ac9e154d6e50f505b2e0c116fbc2d926addf68
qiushicumt <qiushicumt@gmail.com>
2015-10-28 00:24:57 +0800

    C#And.Net Basis Day03 Commit2
    2015-10-27 MayDay26-PC

---
+++ C#And.Net Workspace/C#基础/Day03/Day03/05LuoJiYunSuanFu/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05LuoJiYunSuanFu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  1、逻辑运算符，&&、||和！，分别为与、或、非
+            //  当"&&"符号的两侧的表达式都为true时，整个表达式的结果为true，若有一个为false，则整个表达式结果为false
+            //  当"||"符号的两侧的表达式都为false时，整个表达式的结果为false，若有一个为true，则整个表达式结果为true
+            //  "!"表示取反
+            //  
+        }
+    }
+}
