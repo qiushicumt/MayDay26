d55ce7371bb1d7bbdfd695fda415c1b7091247ee
qiushicumt <qiushicumt@gmail.com>
2015-10-27 23:40:39 +0800

    JavaScript Practise Cookie Commit3
    2015-10-27 MayDay26-PC

---
+++ JavaScript Develop/JS Practise/js/cookie.js
@@ -0,0 +1,18 @@
+﻿// JavaScript source code
+function setCookie(name, value, iDay) {
+    var oDate = new Date();
+    oDate.setDate(oDate.getDate() + iDay);
+    document.cookie = name + "=" + value + ";expires=" + oDate;
+}
+function getCookie(name) {
+    var strArr = document.cookie.split("; ");
+    for (var i = 0; i < strArr.length; i++) {
+        var strArr1 = strArr[i].split("=");
+        if (strArr1[0] == name) {
+            return strArr1[1];
+        }
+    }
+}
+function removeCookie(name) {
+    setCookie(name, '1', -1);
+}
