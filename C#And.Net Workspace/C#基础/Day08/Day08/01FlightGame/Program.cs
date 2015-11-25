c5014e2da06cb6f4d72385b754a6e4126fbed1b3
qiushicumt <qiushicumt@gmail.com>
2015-11-25 16:10:58 +0800

    C#And.Net Basis Day08 Commit2

--- C#And.Net Workspace/C#基础/Day08/Day08/01FlightGame/Program.cs
+++ C#And.Net Workspace/C#基础/Day08/Day08/01FlightGame/Program.cs
@@ -19,6 +19,7 @@ namespace _01FlightGame
             DrawMap();
             Console.ReadKey();
         }
+        
         /// <summary>
         /// 在控制台画游戏头
         /// </summary>
@@ -38,6 +39,7 @@ namespace _01FlightGame
             Console.WriteLine("******************************");
             Console.ForegroundColor = ConsoleColor.Black;
         }
+        
         /// <summary>
         /// 初始化地图
         /// </summary>
@@ -71,18 +73,20 @@ namespace _01FlightGame
                 Maps[timeTravel[i]] = 4;
             }
         }
+        
         /// <summary>
         /// 画地图
         /// </summary>
         public static void DrawMap()
-        {
-            //  画地图时，首先去判断A和B的位置，根据A和B的位置，画出A、B
-            //  然后再画其他关卡
+        { 
+            #region
+            /*         
             //  第一步先画第一横行
-            for(int i = 0; i <　30; i++)
+            for (int i = 0; i <　30; i++)
             {
                 //  PlayerPos[0]存储A的位置，PlayerPos[1]存储B的位置，相等则A和B在同一位置上
                 //  判断PlayerPos[0]与i是否相等，则用于保证A和B一定在第一行上
+                
                 if (PlayerPos[0]==PlayerPos[1] && PlayerPos[0]==i)
                 {
                     Console.ForegroundColor = ConsoleColor.DarkCyan;
@@ -121,9 +125,165 @@ namespace _01FlightGame
                             Console.Write("卐");
                             break;                        
                     }
+                }               
+            }
+
+            //  画第一竖行
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < 29; j++)
+                {
+                    Console.Write("　");
+                }
+                if (PlayerPos[0] == PlayerPos[1] && PlayerPos[0] == (30 + i))
+                {
+
+                    Console.Write("<>");
+                }
+                else if (PlayerPos[0] == (30 + i))
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.Write("Ａ");
+                }
+                else if (PlayerPos[1] == (30 + i))
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.Write("Ｂ");
+                }
+                else
+                {
+                    //Console.ForegroundColor = ConsoleColor.Yellow;
+                    //Console.Write("□");
+                    switch (Maps[30 + i])
+                    {
+                        case 0:
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.Write("□");
+                            break;
+                        case 1:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.Write("◎");
+                            break;
+                        case 2:
+                            Console.ForegroundColor = ConsoleColor.Magenta;
+                            Console.Write("☆");
+                            break;
+                        case 3:
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write("▲");
+                            break;
+                        case 4:
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.Write("卐");
+                            break;
+                    }
                 }
             }
+            */
+            #endregion
+            //  画第一横行
+            for(int i = 0; i < 30; i++)
+            {
+                string mapStr = DrawStringMap(i);
+                Console.Write(mapStr);
+            }
+
+            //  画第一竖行
+            for(int i = 30; i < 35; i++)
+            {
+                Console.WriteLine();
+                for(int j = 0; j < 29; j++)
+                {
+                    Console.Write("  ");
+                }
+                string mapStr = DrawStringMap(i);
+                Console.Write(mapStr);
+            }
+
+            //  画第二横行
+            //  画第二横行前应当先另起一行
+            Console.WriteLine();
+            for(int i = 64; i > 34; i--)
+            {
+                string mapStr = DrawStringMap(i);
+                Console.Write(mapStr);
+            }
+
+            //  画第二竖行
+            for (int i = 65; i < 70; i++)
+            {
+                Console.WriteLine();                
+                string mapStr = DrawStringMap(i);
+                Console.Write(mapStr);
+                for (int j = 0; j < 29; j++)
+                {
+                    Console.Write("  ");
+                }
+            }
+
+            //  画第三横行
+            //  画第三横行前先进行换行
+            Console.WriteLine();
+            for (int i = 70; i < 100; i++)
+            {
+                string mapStr = DrawStringMap(i);
+                Console.Write(mapStr);
+            }
         }
+        
+        /// <summary>
+        /// 判断地图上相应的位置应该画什么样的关卡
+        /// </summary>
+        /// <param name="i">地图上的位置索引</param>
+        /// <returns>返回所应画的字符</returns>
+        public static string DrawStringMap(int i)
+        {
+            //  画地图时，首先去判断A和B的位置，根据A和B的位置，画出A、B
+            //  然后再画其他关卡
+            string str = "";
+            if (PlayerPos[0] == PlayerPos[1] && PlayerPos[0] == i)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                str = "<>";
+            }
+            else if (PlayerPos[0] == i)
+            {
+                str = "Ａ";
+            }
+            else if (PlayerPos[1] == i)
+            {
+                str = "Ｂ";
+            }
+            else
+            {
+                switch (Maps[i])
+                {
+                    case 0:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        str = "□";
+                        break;
+                    case 1:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        str = "◎";
+                        break;
+                    case 2:
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        str = "☆";
+                        break;
+                    case 3:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        str = "▲";
+                        break;
+                    case 4:
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        str = "卐";
+                        break;
+                }
+            }
+            return str;
+        }
+        
         /// <summary>
         /// 
         /// </summary>
