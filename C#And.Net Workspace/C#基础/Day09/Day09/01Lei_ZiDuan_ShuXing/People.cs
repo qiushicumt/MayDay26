d1fd9ffd9682d7c4219be15c5295006184de11ea
qiushicumt <qiushicumt@gmail.com>
2015-11-30 23:03:49 +0800

    C#And.Net Basis Day09 Commit2

---
+++ C#And.Net Workspace/C#基础/Day09/Day09/01Lei_ZiDuan_ShuXing/People.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01Lei_ZiDuan_ShuXing
+{
+    class People
+    {
+        //  声明People类的字段(成员变量),类的成员变量在默认情况下是private访问属性
+        string _name;
+        int _age;
+        char _gender;
+
+        //  声明类的属性
+        //  类的属性中，get方法是当获取对象的变量值时被调用，set方法则是在设置对象的变量值时被调用
+        //  可以在get方法或者set方法中对字段进行设置相关的限制条件
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }      //  value为对字段设置值时所设置的值
+        }
+        public char Gender
+        {
+            get { return _gender; }
+            set 
+            {
+                if (value != '男' && value != '女')
+                    value = '男';
+                _gender = value;
+            }
+        }
+        public int Age
+        {
+            get     //  在get方法中对_age进行限制
+            {
+                if (_age < 0)
+                    _age = 0;
+                if (_age > 100)
+                    _age = 100;
+                return _age;
+            }
+            set
+            {
+                _age = value;
+            }
+        }
+
+        //  声明People类的方法
+        public void Show()
+        {
+            Console.WriteLine("我是{0}，我是{1}生，我今年{2}岁。", this.Name, this.Gender, this.Age);
+        }
+
+    }
+}
---
+++ C#And.Net Workspace/C#基础/Day09/Day09/01Lei_ZiDuan_ShuXing/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01Lei_ZiDuan_ShuXing
+{
+    //  类是一种抽象的集合，具有继承、封装、多态的特性
+    //  类包含类的字段(又叫成员变量)、属性、方法
+    //  在类中，字段一般用于存储数据，所以多设置为private访问属性
+    //  想要修改字段值，就要通过类的属性对字段进行修改
+    //  而属性本身不存储数据
+    //  类的方法是用于设置描述类所拥有的行为
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  声明一个People类的对象
+            People onePerson = new People();    
+            //  通过对象的属性，对私有变量进行赋值
+            onePerson.Name = "张三";
+            onePerson.Gender = '男';
+            onePerson.Age = 23;
+
+            People twoPerson = new People();
+            twoPerson.Name = "韩梅梅";
+            twoPerson.Gender = '女';
+            twoPerson.Age = 28;
+
+            People threePerson = new People();
+            threePerson.Name = "李宇春";
+            threePerson.Gender = '春';
+            threePerson.Age = -30;
+
+            onePerson.Show();
+            twoPerson.Show();
+            threePerson.Show();
+            Console.ReadKey();
+        }
+    }
+}
