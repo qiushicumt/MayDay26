5537093d5011bad16aaf0522164f6583fa54d276
qiushicumt <qiushicumt@gmail.com>
2015-12-01 22:55:44 +0800

    C#And.Net Basis Day09 Commit4

---
+++ C#And.Net Workspace/C#基础/Day09/Day09/05Class Practise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05Class_Practise
+{
+    /*
+     * 写一个Ticket类,有一个距离属性(本属性只读,在构造方法中赋值),不能为负数,有一个价格属性,价格属性只读,并且根据距离distance计算价格Price (1元/公里):        0-100公里        票价不打折        101-200公里    总额打9.5折        201-300公里    总额打9折        300公里以上    总额打8折        有一个方法,可以显示这张票的信息.90公里90块钱
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ticket myTicket = new Ticket(400);
+            myTicket.ShowPrice();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#And.Net Workspace/C#基础/Day09/Day09/05Class Practise/Ticket.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05Class_Practise
+{
+    public class Ticket
+    {
+        int _distance;
+        double _price;
+        //  Distance属性和Price属性均只有get方法，即两个属性都是只能获取数值，不能在外部对属性进行赋值
+        public int Distance
+        {
+            get 
+            {
+                if (_distance < 0)
+                    return _distance = 0;
+                return _distance; 
+            }
+        }
+        public double Price
+        {
+            get { return _price; }
+        }        
+        //  类的对象的构造函数，包含一个参数，参数用于给属性Distance赋值
+        public Ticket(int distance)
+        {
+            this._distance = distance;
+        }
+        /// <summary>
+        /// 根据用户输入的距离，计算票价并显示价格信息.
+        /// </summary>
+        public void ShowPrice()
+        {
+            if (this.Distance >= 0 && this.Distance <= 100)
+            {
+                this._price = this.Distance * 1;
+            }
+            else if (this.Distance > 100 && this.Distance <= 200)
+            {
+                this._price = this.Distance * 1 * 0.95;
+            }
+            else if (this.Distance > 200 && this.Distance <= 300)
+            {
+                this._price = this.Distance * 1 * 0.9;
+            }
+            else
+            {
+                this._price = this.Distance * 1 * 0.8;
+            }
+            Console.WriteLine("这张票的价格信息为，{0}公里{1}元钱", this.Distance, this.Price);
+        }
+    }
+}
