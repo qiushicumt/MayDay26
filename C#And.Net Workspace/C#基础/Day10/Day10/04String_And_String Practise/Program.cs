caeded07a996a94f08aa8667d65ca3e98942e251
qiushicumt <qiushicumt@gmail.com>
2015-12-07 22:49:36 +0800

    C#And.Net Basis Day10 Commit3

--- C#And.Net Workspace/C#基础/Day10/Day10/04String_And_String Practise/Program.cs
+++ C#And.Net Workspace/C#基础/Day10/Day10/04String_And_String Practise/Program.cs
@@ -38,6 +38,151 @@ namespace _04String_And_String_Practise
             Console.ReadKey();
         }
     }
+    
+    //  练习3、两个学员输入各自最喜欢的课程名称，判断是否一致,如果相等,则输出你们俩喜欢相同的课程.如果不相同,则输出你们俩喜欢不相同的课程.
+    //  上题中两位同学输入 c#和 C#代表同一门课,怎么办?
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("第一位同学，请输入喜欢的课程:");
+            string courseOne = Console.ReadLine();
+            Console.Write("第二位同学，请输入喜欢的课程:");
+            string courseTwo = Console.ReadLine();
+            //  ToLower()方法用于将字符串全部转换为小写，ToUper()方法用于将字符串全部转换为大写
+            courseOne = courseOne.ToLower();
+            courseTwo = courseTwo.ToLower();
+            if (courseOne == courseTwo)
+            {
+                Console.WriteLine("你们喜欢相同的课程！");
+            }
+            else
+            {
+                Console.WriteLine("你们喜欢不相同的课程！");
+            }
+
+            Console.ReadKey();
+        }
+    }
     */
-    //  练习3、两个学员输入各自最喜欢的课程名称，判断是否一致,如果相等,则输出你们俩喜欢相同的课程.如果不相同,则输出你们俩喜欢不相同的课程.    //  上题中两位同学输入 c#和 C#代表同一门课,怎么办?
+
+    //  练习4、字符串的各种方法   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("请输入一个字符串:");
+            string str1 = Console.ReadLine();
+            Console.WriteLine("字符串的长度为：{0}", str1.Length);     //  str.Length：返回字符串的长度
+
+            Console.Write("请输入英文字符串:");
+            string str2 = Console.ReadLine();
+            Console.WriteLine("输入的是：{0}，全部转换为大写为：{1}", str2, str2.ToUpper());       //  strB.ToUper()方法用于将所有字母转换为大写字母
+            Console.WriteLine("全部转换为小写为：{0}", str2.ToLower());      //  strA.ToLower()方法用于将所有字母转换为小写字母
+
+            bool b = Equals(str1, str2);        //  Equals(A, B)方法可以用于比较两个字符串是否相等，相等返回true，不等返回false
+            if (b)
+            {
+                Console.WriteLine("两次输入的字符串相同。");
+            }
+            else
+            {
+                Console.WriteLine("两次输入的字符串不相同。");
+            }
+
+            Console.Write("请输入带空格的字符串:");
+            string str3 = Console.ReadLine();
+            char[] myChar = { ' ' };
+            //  strA.Split(char[] myChar)方法用于对字符串进行分割，需要传入一个char[]数组，char[]数组为一组分隔符，方法返回分割后的子字符串组成的数组
+            string[] myStrs = str3.Split(myChar);           
+            Console.WriteLine("分割后的子字符串分别为：");
+            for (int i = 0; i < myStrs.Length; i++)
+            {
+                Console.WriteLine(myStrs[i]);
+            }
+
+            Console.Write("输入一个字符串：");
+            string str4 = Console.ReadLine();
+            string str5 = str4.Substring(2);        //  strA.Substring(int)方法用于从字符串中截取出子字符串，传入的参数为想要截取的起始位置，返回从起始位置开始到末尾的子字符串
+            Console.WriteLine("截取后的子字符串为：{0}", str5);
+
+            Console.Write("输入一个字符串：");
+            string str6 = Console.ReadLine();
+            char myChar1 = 'a';
+            string str7 = "abc";
+            int numA = str6.IndexOf(myChar1);          //  strA.IndexOf(char1)方法用于返回字符在当前字符串中出现的起始位置，若没有，则返回-1
+            int numB = str6.IndexOf(str7);             //  strA.IndexOf(strB)方法返回字符串在当前字符串中出现的起始位置，若没有，则返回-1
+            int numC = str6.LastIndexOf(myChar1);      //  strA.LastIndexOf(char1)方法返回字符在当前字符串中出现的最后的位置，没有出现，则返回-1
+            int numD = str6.LastIndexOf(str7);         //  strA.LastIndexOf(strB)方法返回字符串在当前字符串中出现的最后的位置，若无，则返回-1
+
+            if (numA != -1)
+            {
+                Console.WriteLine("字符a在字符串{0}中出现的起始位置为：{1}", str6, numA);
+            }
+            else
+            {
+                Console.WriteLine("字符a没有出现在字符串{0}中", str6);
+            }
+            if (numB != -1)
+            {
+                Console.WriteLine("字符串abc在字符串{0}中出现的起始位置为：{1}", str6, numB);
+            }
+            else
+            {
+                Console.WriteLine("字符abc没有出现在字符串{0}中", str6);
+            }
+            if (numC != -1)
+            {
+                Console.WriteLine("字符a在字符串{0}中最后出现的位置为：{1}", str6, numC);
+            }
+            else
+            {
+                Console.WriteLine("字符a没有出现在字符串{0}中", str6);
+            }
+            if (numD != -1)
+            {
+                Console.WriteLine("字符串abc在字符串{0}中最后出现的位置为：{1}", str6, numD);
+            }
+            else
+            {
+                Console.WriteLine("字符a没有出现在字符串{0}中", str6);
+            }
+
+            if (str6.StartsWith(str7))      //  strA.StartsWith(strB)方法用于判断当前字符串是否以指定的字符串开头的，是，则返回true，否，则返回false
+            {
+                Console.WriteLine("字符串{0}是以字符串{1}开头的。", str6, str7);
+            }
+            else
+            {
+                Console.WriteLine("字符串{0}不是以字符串{1}开头的。", str6, str7);
+            }
+            if (str6.EndsWith(str7))        //  strA.EndsWith(strB)方法用于判断当前字符串是否以指定的字符串结尾的，是，则返回true，否，则返回false
+            {
+                Console.WriteLine("字符串{0}是以字符串{1}结尾的。", str6, str7);
+            }
+            else
+            {
+                Console.WriteLine("字符串{0}不是以字符串{1}结尾的。", str6, str7);
+            }
+
+            Console.Write("请输入字符串：");
+            string str8 = Console.ReadLine();
+            string str9 = str8.Substring(2, 3);         //  从原字符串中截取一个长度为3的子字符串
+            str8.Replace(str9, "abc");                  //  strA.Replace(strB, strC)方法用于将字符串中的子字符串替换为指定的字符串 
+            Console.WriteLine("替换后的字符串为：{0}", str8);
+
+            Console.WriteLine("请输入字符串：");
+            string str10 = Console.ReadLine();
+            if (str10.Contains("abc"))                  //  strA.Contains(strB)方法用于判断字符串是否包含指定的字符串，是，则返回true，否，则返回false
+            {
+                Console.WriteLine("字符串{0}中包含字符串\"abc\"");
+            }
+            else
+            {
+                Console.WriteLine("字符串{0}中不包含字符串\"abc\"");
+            }
+
+            Console.ReadKey();
+        }
+    }
 }
---
+++ C#And.Net Workspace/C#基础/Day10/Day10/05String_Practise/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _05String_Practise
+{
+    /*
+    //  练习1:接收用户输入的字符串，将其中的字符以与输入相反的顺序输出。"abc"→"cba"
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("请输入一个字符串:");
+            string myStr1 = Console.ReadLine();
+            string myStr2 = SortOrder(myStr1);
+            Console.WriteLine("字符串{0}反序后的字符串为：{1}", myStr1, myStr2);
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// 将传入的字符串反序输出
+        /// </summary>
+        /// <param name="str1">传入的字符串</param>
+        /// <returns>返回反序后的字符串</returns>
+        public static string SortOrder(string str1)
+        {
+            //  声明一个StringBuilder对象，按照反向顺序接收string对象的字符
+            StringBuilder str2 = new StringBuilder();
+            for (int i = (str1.Length - 1); i >= 0; i--)
+            {
+                str2.Append(str1[i]);
+            }
+            return str2.ToString();
+        }
+    }
+    
+    //  练习2：接收用户输入的一句英文，将其中的单词以反序输出。"hello c sharp"→"sharp c hello"
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("请输入一句英文：");
+            string englishStr1 = Console.ReadLine();
+            string englishStr2 = SortString(englishStr1);
+            Console.WriteLine("反序后的英文语句为：{0}", englishStr2);
+            Console.ReadKey();
+        }
+        public static string SortString(string str1)
+        {
+            StringBuilder str2 = new StringBuilder();
+            char[] splitChar = {' '};
+            string[] myStr = str1.Split(splitChar);
+            for (int i = (myStr.Length - 1); i >= 0; i--)
+            {
+                str2.Append(myStr[i]);
+                str2.Append(' ');
+            }
+            string temp = str2.ToString();
+            temp.TrimEnd(splitChar);
+            return temp;
+        }
+    }
+    
+    //  练习3：从Email中提取出用户名和域名：abc@163.com
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("请输入你的Email:");
+            string myEmail = Console.ReadLine();
+            string userName = "";
+            string domain = "";
+            emailTiQu(myEmail, out userName, out domain);
+            Console.WriteLine("用户名为：{0}", userName);
+            Console.WriteLine("域名为：{0}", domain);
+            Console.ReadKey();
+        }
+        public static void emailTiQu(string str1, out string userName, out string domain)
+        {
+            char[] myChar = { '@' };
+            string[] myString = str1.Split(myChar);
+            userName = myString[0];
+            domain = myString[1];
+        }
+    }
+     * 
+    //  练习6：让用户输入一句话,判断这句话中有没有邪恶,如果有邪恶就替换成这种形式然后输出,如:老牛很邪恶,输出后变成老牛很**;
+    class Program
+    {
+        static void Main()
+        {            
+            //Console.WriteLine("请输入一句话：");
+            //string myStr1 = Console.ReadLine();
+            //bool flag = myStr1.Contains("邪恶");
+            //if (flag)
+            //{
+            // * 错误：，没有返回给myStr1
+            //    myStr1.Replace("邪恶", "**");
+            //}
+            //Console.WriteLine(myStr1);
+            Console.Write("请输入一句话：");
+            string myStr1 = Console.ReadLine();
+            if (myStr1.Contains("邪恶"))
+            {
+                myStr1 = myStr1.Replace("邪恶", "**");
+            }
+            Console.WriteLine(myStr1);
+            Console.ReadKey();
+        }
+    }
+    
+    //  练习7：把{“诸葛亮”,”鸟叔”,”卡卡西”,”卡哇伊”}变成诸葛亮|鸟叔|卡卡西|卡哇伊,然后再把|切割掉
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] myStr1 = { "诸葛亮", "鸟叔", "卡卡西", "卡哇伊" };
+            string myStr2 = "";
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < (myStr1.Length-1); i++)
+            {
+                sb.Append(myStr1[i]);
+                sb.Append('|');
+            }
+            sb.Append(myStr1[myStr1.Length - 1]);
+            myStr2 = sb.ToString();
+            Console.WriteLine(myStr2);
+            char[] myChar = { '|' };
+            myStr1 = myStr2.Split(myChar, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < myStr1.Length; i++)
+            {
+                Console.WriteLine(myStr1[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+    */
+    //  练习4：文本文件中存储了多个文章标题、作者，标题和作者之间用若干空格（数量不定）隔开，每行一个，标题有的长有的短，输出到控制台的时候最多标题长度10，如果超过10，则截取长度8的子串并且最后添加“...”，加一个竖线后输出作者的名字。
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\myGitHub\C#And.Net Workspace\C#基础\Day10\Day10\1.txt";      //  在字符串前加上@符号，表示忽略字符串中的转移符
+            string[] myTxt = File.ReadAllLines(path);              //  File.ReadAllLines()方法用于打开指定路径下的文本文档，返回读取的内容的字符串数组，然后关闭
+            Console.ReadKey();
+        }
+    }
+    //  练习5：让用户输入一句话,找出所有e的位置
+
+}
