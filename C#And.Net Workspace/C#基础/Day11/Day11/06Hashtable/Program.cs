9d4626a553388d9e84f4c096a9aa90e2226e2cf1
qiushicumt <qiushicumt@gmail.com>
2015-12-10 20:46:54 +0800

    C#And.Net Basis Day11 Commit4

---
+++ C#And.Net Workspace/C#基础/Day11/Day11/06Hashtable/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06Hashtable
+{
+    //  Hashtable，是一种数据集合，以键值对的形式存储数据。集合中的每一个键对应一个值，值可以为null
+    //  注：在键值对中，键必须是唯一的，值可以相同
+    //  Add(item, value)方法用于给Hashtable添加数据，键、值都为object类型，即任一类型。使用此方法，当item已经存在时，程序会报错
+    //  collection[item] = value方式也可以为键值对进行添加数据的操作。使用次方法，当item已存在时，原本的值会被新的值覆盖掉
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add(1, 12);
+            ht.Add(2, "张三");
+            ht.Add(3, '男');
+            ht.Add(false, "错误");
+            ht[4] = 5000m;
+            ht[2] = "赵四";               //  原值张三被覆盖掉
+            //  foreach循环多用于遍历集合
+            //  for(var item in collection)
+            //  遍历键值对中的键
+            //foreach (var j in ht.Keys)
+            //{
+            //    Console.WriteLine(j);
+            //}
+            //  遍历键值对中的值
+            foreach (var i in ht.Values)
+            {
+                Console.WriteLine(i);
+            }
+            //  foreach()循环中设置循环条件为循环集合中的键，值则由collection[item]的下标方式给出
+            foreach (var item in ht.Keys)
+            {
+                Console.WriteLine("键是{0}--------值是{1}", item, ht[item]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#And.Net Workspace/C#基础/Day11/Day11/07Hashtable_Practise/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace _07Hashtable_Practise
+{
+    //  练习：将用户输入的简体字转换为繁体字
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string JIANTI = "你说那边鲜花还在开所以我要去看和大海风从面吹过来的心也被暖阳光洒满金色爱空中飘散鸟呼唤裙角摇摆北方等着到背上行囊就离";
+            const string FANTI = "你說那邊鮮花還在開所以我要去看和大海風從面吹過來的心也被暖陽光灑滿金色愛空中飄散鳥呼喚裙角搖擺北方等著到背上行囊就離";
+
+            Hashtable myHashtable = new Hashtable();    //  声明一个hashtable对象实例
+            for (int i = 0; i < JIANTI.Length; i++)     //  for循环将键值对添加到hashtable对象中
+            {
+                myHashtable.Add(JIANTI[i], FANTI[i]);
+            }
+
+            Console.WriteLine("请输入一句话：");
+            string txt = Console.ReadLine();
+            foreach (var item in txt)
+            {
+                if (myHashtable.ContainsKey(item))
+                {
+                    Console.Write(myHashtable[item]);
+                }
+                else
+                {
+                    Console.Write(item);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
