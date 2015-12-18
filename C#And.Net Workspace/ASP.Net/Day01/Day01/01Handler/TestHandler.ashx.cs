5981aa27aa9013881b3c8ab1148ffedb7f2288a6
qiushicumt <qiushicumt@gmail.com>
2015-12-18 20:39:42 +0800

    C#And.Net WebDevelop Day01 Commit1

---
+++ C#And.Net Workspace/ASP.Net/Day01/Day01/01Handler/TestHandler.ashx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _01Handler
+{
+    /// <summary>
+    /// TestHandler 的摘要说明
+    /// </summary>
+    public class TestHandler : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            //context.Response.ContentType = "text/plain";
+            context.Response.ContentType = "text/html";
+
+            //  context.Request得到用户通过浏览器向服务器发出的请求
+            //  string name = context.Request["name"];
+            //  int age = Convert.ToInt32(context.Request["age"]);
+            string username = context.Request["username"];
+            string password = context.Request["password"];
+
+            context.Response.Write("用户名是："+username+", 密码是："+password);
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
