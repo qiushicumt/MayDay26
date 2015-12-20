9b0352172e9c23dc7774787c850ff3abe3e7b197
qiushicumt <qiushicumt@gmail.com>
2015-12-20 23:26:21 +0800

    C#And.Net WebDevelop Day02 Commit1

---
+++ C#And.Net Workspace/ASP.Net/Day02/01NVelocity/handlers/loginHandler.ashx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _01NVelocity.handlers
+{
+    /// <summary>
+    /// loginHandler 的摘要说明
+    /// </summary>
+    public class loginHandler : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/html";
+
+            string userName = context.Request["username"];
+            string passWord = context.Request["password"];
+            if (string.IsNullOrEmpty(userName) && string.IsNullOrEmpty(passWord))
+            {
+
+            }
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
