beb4e4789efbda586f58c95c7ff10f0c1b20010f
qiushicumt <qiushicumt@gmail.com>
2015-12-24 00:18:27 +0800

    C#And.Net WebDevelop Day03 Commit1

---
+++ C#And.Net Workspace/ASP.Net/Day03/01DataBind/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _01DataBind
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Person pers = new Person();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //  在xaml文件中，在想要绑定数据的控件添加属性绑定，如 Text{Binding Name}  表示将当前控件的Text属性值与绑定的数据上下文的Name属性关联，即 Text=Name
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {           
+            pers.Name = "赵四";
+            pers.Age = 43;
+
+            //  DataContexts属性表示绑定的数据上下文
+            txtName.DataContext = pers;
+            txtAge.DataContext = pers;
+
+        }
+
+        private void ageAddButton_Click(object sender, RoutedEventArgs e)
+        {
+            pers.Age++;
+        }
+
+        private void showButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(pers.Age.ToString());
+        }
+    }
+}
---
+++ C#And.Net Workspace/ASP.Net/Day03/01DataBind/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01DataBind
+{
+    //  INotifyPropertyChanged接口用于在属性值发生改变时向客户端发出通知
+    class Person:INotifyPropertyChanged
+    {
+        string _name;
+        int _age;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set 
+            { 
+                _age = value;
+                if (PropertyChanged != null)    //  在事件触发前与null进行比较，查看是否包含事件处理程序，如果==null，不包含，则不触发事件
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("Age"));     // new PropertyChangedEventArgs(属性名)，表示当哪个属性改变时发出通知
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
