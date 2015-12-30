9a65eef427bf8324bfd84762f4f95f3c56a793dd
qiushicumt <qiushicumt@gmail.com>
2015-12-30 17:21:40 +0800

    C#And.Net WebDevelop Day04 Commit1

---
+++ C#And.Net Workspace/ASP.Net/Day04/01CookieLogin/handlers/UserLogin.ashx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _01CookieLogin.handlers
+{
+    /// <summary>
+    /// UserLogin 的摘要说明
+    /// </summary>
+    public class UserLogin : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/html";
+            string login = context.Request["login"];
+            if (String.IsNullOrEmpty(login))
+            {
+            }
+            else
+            {
+            }
+
+            context.Response.Write("Hello World");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
