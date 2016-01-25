01df31ac970aca54cb11ee2d1ca6d7ea2cf079e9
qiushicumt <qiushicumt@gmail.com>
2016-01-25 17:32:27 +0800

    C#And.Net WebDevelop Day07 Commit4

---
+++ C#And.Net Workspace/ASP.Net/Day07/Chemical_Plant_Pro_New/handlers/productList.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="productList.ashx.cs" Class="Chemical_Plant_Pro_New.handlers.ProductList" %>
---
+++ C#And.Net Workspace/ASP.Net/Day07/Chemical_Plant_Pro_New/handlers/productList.ashx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Chemical_Plant_Pro_New.handlers
+{
+    /// <summary>
+    /// ProductList 的摘要说明
+    /// </summary>
+    public class ProductList : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/html";
+            string html = NVelocity.ReturnHtml("Admin/productList.html", null);
+            context.Response.Write(html);
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
