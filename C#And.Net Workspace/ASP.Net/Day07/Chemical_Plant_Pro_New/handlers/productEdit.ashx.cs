0c3a55ec73002d30fa1823cd6cf81a0722d8ca59
qiushicumt <qiushicumt@gmail.com>
2016-02-01 17:32:11 +0800

    C#And.Net WebDevelop Day07 Commit7

--- C#And.Net Workspace/ASP.Net/Day07/Chemical_Plant_Pro_New/handlers/productEdit.ashx.cs
+++ C#And.Net Workspace/ASP.Net/Day07/Chemical_Plant_Pro_New/handlers/productEdit.ashx.cs
@@ -28,34 +28,66 @@ namespace Chemical_Plant_Pro_New.handlers
                 if (action == "addNew")
                 {
                     string save = context.Request["save"];
-                    if (String.IsNullOrEmpty(save))
+                    if (String.IsNullOrEmpty(save))             //  sava为空，则页面为新增展示页面
                     {
-                        DataTable dtProCategory = SQLHelper.ExecuteDataTable("select * from T_ProductCategory");
-                        var data = new { Action = "addNew", ProCategories = dtProCategory.Rows };
+                        DataTable dtCategory = SQLHelper.ExecuteDataTable("select * from T_ProductCategory");
+                        var data = new { Action = "addNew", productCategories = dtCategory.Rows };
                         html = NVelocity.ReturnHtml("Admin/productEdit.html", data);
                         context.Response.Write(html);
                     }
                     else
                     {
-                        string proName = context.Request["productName"];
-                        string proCategory = context.Request["proCategory"];
-                        //  获取表单上传的图片
-                        HttpPostedFile postImage = context.Request.Files["proImage"];
-                        Guid myGuid = new Guid();                        
-                        string imgName = myGuid.ToString() + Path.GetExtension(postImage.FileName);
-                        string imgFilePath = context.Server.MapPath("~/images/");
-                        postImage.SaveAs(imgFilePath + imgName);
-
-                        string proImgPath = "../images/productImg/" + imgName;
+                        //  获取页面传递的表单中的值
+                        string productName = context.Request["productName"];                
+                        int proCategoryId = Convert.ToInt32(context.Request["proCategory"]);
                         string proDescription = context.Request["proDescription"];
 
-                        DataTable dtProCategory = SQLHelper.ExecuteDataTable("select * from T_ProductCategory");
+                        //  获取上传的图片
+                        HttpPostedFile imgFile = context.Request.Files["proImage"];
+                        Guid newGuid = Guid.NewGuid();
+                        string fileName = newGuid.ToString() + Path.GetExtension(imgFile.FileName);
+                        string filePath = context.Server.MapPath("~/images/productImg/");
+                        imgFile.SaveAs(filePath + fileName);
+                        string imgPath = "../images/productImg/" + fileName;
 
-                        SQLHelper.ExecuteNonQuery("insert into T_Products(Name, ImagePath, ProductDescription, )");
-                    }                                       
+                        SQLHelper.ExecuteNonQuery("insert into T_Products (Name, ImagePath, ProductDescription, CategoryId) values(@Name, @ImagePath, @ProductDescription, @CategoryId)",
+                            new SqlParameter("@Name", productName),
+                            new SqlParameter("@ImagePath", imgPath),
+                            new SqlParameter("@ProductDescription", proDescription),
+                            new SqlParameter("@CategoryId", proCategoryId));
+
+                        context.Response.Redirect("productList.ashx");
+                    }
                 }
                 else if (action == "edit")
                 {
+                    string save = context.Request["save"];
+                    if (String.IsNullOrEmpty(save))             //  编辑显示
+                    {
+                        int id = Convert.ToInt32(context.Request["Id"]);
+                        DataTable dtProducts = SQLHelper.ExecuteDataTable("select * from T_Products where Id=@Id", new SqlParameter("@Id", id));
+                        if (dtProducts.Rows.Count <= 0)
+                        {
+                            context.Response.Write("产品ID为" + id + "的数据不存在！");
+                        }
+                        else if (dtProducts.Rows.Count > 1)
+                        {
+                            context.Response.Write("产品ID为" + id + "的数据存在多条！");
+                        }
+                        else
+                        {
+                            DataRow theProduct = dtProducts.Rows[0];
+                            DataTable dtProCategories = SQLHelper.ExecuteDataTable("select * from T_ProductCategory");
+                            var data = new { Action = "edit", Product = theProduct, ProCategories = dtProCategories.Rows };
+                            html = NVelocity.ReturnHtml("Admin/productEdit.html", data);
+                            context.Response.Write(html);
+                        }
+                    }
+                    else
+                    {
+                                
+                    }
+                    
                 }
                 else if (action == "delete")
                 {
