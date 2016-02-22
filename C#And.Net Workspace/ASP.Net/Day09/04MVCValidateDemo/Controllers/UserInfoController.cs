9b3e09ec10d658160787bbe518e321bf5e610b4a
qiushicumt <qiushicumt@gmail.com>
2016-02-22 17:51:13 +0800

    C#And.Net WebDevelop Day09 Commit4

---
+++ C#And.Net Workspace/ASP.Net/Day09/04MVCValidateDemo/Controllers/UserInfoController.cs
@@ -0,0 +1,35 @@
+﻿using _04MVCValidateDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _04MVCValidateDemo.Controllers
+{
+    public class UserInfoController : Controller
+    {
+        //
+        // GET: /UserInfo/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Add(UserInfo user)
+        {
+            if (ModelState.IsValid)     //  ModelState.IsValid表示是否验证，验证成功为true
+            {
+                Response.Write("<script>alert('验证成功')</script>");
+            }
+            return View();
+        }
+        
+	}
+}
---
+++ C#And.Net Workspace/ASP.Net/Day09/04MVCValidateDemo/Models/UserInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace _04MVCValidateDemo.Models
+{
+    public class UserInfo
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage="*为必填项")]
+        [StringLength(8, ErrorMessage="用户名长度必须<8")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "*为必填项")]
+        [Range(16, 120, ErrorMessage="年龄范围是16~120")]
+        [RegularExpression(@"^\d*$", ErrorMessage="年龄必须是数字")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "*为必填项")]
+        public string UserEmail { get; set; }
+    }
+}
