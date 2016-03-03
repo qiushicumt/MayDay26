109c6ee477fd24c9a0f2abc77028f8bac6ad9798
qiushicumt <qiushicumt@gmail.com>
2016-03-03 22:31:06 +0800

    C#And.Net WebDevelop Day10 Commit3

---
+++ C#And.Net Workspace/ASP.Net/Day10/01GuestbookPro/Controllers/GuestbookController.cs
@@ -0,0 +1,36 @@
+﻿using _01GuestbookPro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _01GuestbookPro.Controllers
+{
+    public class GuestbookController : Controller
+    {
+        private GuestbookContext _db = new GuestbookContext();
+
+        public ActionResult Index()
+        {
+            //var guestbookEntry = (from entry orderby )
+            var mostRecentEntries = (from entry in _db.Entries orderby entry.DateAdded descending select entry).Take(20);
+            ViewBag.Entries = mostRecentEntries.ToList();
+            return View("GuestbookInfo");
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(GuestbookEntry entry)
+        {
+            entry.DateAdded = DateTime.Now;
+            _db.Entries.Add(entry);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
