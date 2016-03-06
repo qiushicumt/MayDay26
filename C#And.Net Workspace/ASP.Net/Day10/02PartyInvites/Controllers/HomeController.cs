4099b97eb6354c2b4ce02fc736e5d50f3c6a62a3
qiushicumt <qiushicumt@gmail.com>
2016-03-06 16:45:47 +0800

    C#And.Net WebDevelop Day10 Commit4

---
+++ C#And.Net Workspace/ASP.Net/Day10/02PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using _02PartyInvites.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _02PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            string greetingStr = hour < 12 ? "Good morning" : "Good afternoon";
+            ViewBag.Greeting = greetingStr;
+
+            return View();
+        }
+
+        public ActionResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RsvpForm(GuestResponse guestresponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guestresponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ C#And.Net Workspace/ASP.Net/Day10/02PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace _02PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage="Please enter your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage="Please enter your email")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage="Please enter a valid email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage="Please enter your phone number")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage="Please specify your choose")]
+        public bool? WillAttend { get; set; }       //bool?表示WillAttend属性是可以为空的bool类型
+    }
+}
