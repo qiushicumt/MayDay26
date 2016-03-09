5c90a3051b8645a04435976575d56e1ba63be664
qiushicumt <qiushicumt@gmail.com>
2016-03-09 23:02:54 +0800

    C#And.Net WebDevelop Day11 Commit2

---
+++ C#And.Net Workspace/ASP.Net/SportsStore/SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Ninject;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectControllerFactory:DefaultControllerFactory
+    {
+        private IKernel ninjectKernel;
+        public NinjectControllerFactory()
+        {
+            ninjectKernel = new StandardKernel();
+            AddBindings();
+        }
+
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            return controllerType == null ? null : (IController)ninjectKernel.Get(controllerType);
+        }
+
+        private void AddBindings()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product>{
+                new Product{ProductName="Football", Price=25},
+                new Product{ProductName="Surf board", Price=179},
+                new Product{ProductName="Running shoes", Price=95}
+            }.AsQueryable());       //AsQueryable()方法用于将IEnumerable<T>转换成IQueryable<T>
+            ninjectKernel.Bind<IProductRepository>().ToConstant(mock.Object);
+        }
+    }
+}
