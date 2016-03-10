eb6bad2df6864473e8e50201f2ef8e4334bd162f
qiushicumt <qiushicumt@gmail.com>
2016-03-10 23:13:40 +0800

    C#And.Net WebDevelop SportsStore Commit1

---
+++ C#And.Net Workspace/ASP.Net/SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using SportsStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;          //实例化一个存储库实例对象
+
+        public ProductController(IProductRepository productRepository)
+        {
+            this.repository = productRepository;
+        }
+
+        public ActionResult List()
+        {
+            return View(repository.Products);
+        }
+
+    }
+}
