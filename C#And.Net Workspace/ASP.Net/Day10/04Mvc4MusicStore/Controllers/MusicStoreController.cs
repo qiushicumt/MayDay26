6cc53f8dd958b958ae9073c7a92d526a994e9c13
qiushicumt <qiushicumt@gmail.com>
2016-03-14 09:31:17 +0800

    C#And.Net WebDevelop Day10 Commit7

---
+++ C#And.Net Workspace/ASP.Net/Day10/04Mvc4MusicStore/Controllers/MusicStoreController.cs
@@ -0,0 +1,33 @@
+﻿using _04Mvc4MusicStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _04Mvc4MusicStore.Controllers
+{
+    public class MusicStoreController : Controller
+    {
+        
+        private MusicStoreEntities musicstoreDB = new MusicStoreEntities();
+
+        public ActionResult Index()
+        {
+            var genres = musicstoreDB.Genres.ToList();
+            return View(genres);
+        }
+
+        public ActionResult Browse(string genre)
+        {
+            var genreModel = musicstoreDB.Genres.Include("Albums").Single(g=>g.GenreName == genre);
+
+            return View(genreModel);
+        }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+    }
+}
