fcb4a85427ed72e333578396706c6af05fcc5a4e
qiushicumt <qiushicumt@gmail.com>
2016-03-17 00:09:30 +0800

    C#And.Net WebDevelop ShoppingPro Commit3

--- C#And.Net Workspace/ASP.Net/MvcShoppingPro/MvcShoppingPro/Controllers/HomeController.cs
+++ C#And.Net Workspace/ASP.Net/MvcShoppingPro/MvcShoppingPro/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MvcShoppingPro.Models;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,18 +13,39 @@ namespace MvcShoppingPro.Controllers
         #region 商品分类
         public ActionResult Index()
         {
-            return View();
+            //传递商品类别数据
+            List<ProductCategory> proCat = new List<ProductCategory>() { 
+                new ProductCategory() { Id = 1, Name = "文具" },
+                new ProductCategory() { Id = 2, Name = "礼品" },
+                new ProductCategory() { Id = 3, Name = "书籍" },
+                new ProductCategory() { Id = 4, Name = "电子产品" }
+            };
+            return View(proCat);
         }
         #endregion
 
-        public ActionResult ProductList()
+        #region 商品列表
+        public ActionResult ProductList(int Id)
         {
-            return View();
-        }
+            //传递商品数据
+            var productCat = new ProductCategory() { Id = Id, Name = "类别" + Id };
+            var products = new List<Product>()
+            {
+                new Product(){ Id=1,Category=productCat, Name="原子笔", Description="N/A",Price=30.00M, PublishOn = DateTime.Now, ProductColor = Color.Black },
+                new Product(){ Id = 1, Category = productCat, Name = "铅笔", Description = "N/A", Price = 5M, PublishOn = DateTime.Now, ProductColor = Color.Green }
+            };
 
-        public ActionResult ProductDetail()
+            return View(products);
+        }
+        #endregion
+
+        #region 商品详情
+        public ActionResult ProductDetail(int Id)
         {
+
             return View();
         }
+        #endregion
+        
     }
 }
